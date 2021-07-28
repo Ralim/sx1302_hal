6c94939654b15a10a74e45a0437bee799daef885
Ben V. Brown <Ralim@Ralimtek.com>
2021-07-28 19:28:08 +1000

    Setting up paths

--- tools/reset_lgw.sh
+++ tools/reset_lgw.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script is intended to be used on SX1302 CoreCell platform, it performs
+# This script is intended to be used on SX1302 PineDIO platform, it performs
 # the following actions:
 #       - export/unpexort GPIO23 and GPIO18 used to reset the SX1302 chip and to enable the LDOs
 #       - export/unexport GPIO22 used to reset the optional SX1261 radio used for LBT/Spectral Scan
@@ -44,12 +44,12 @@ init() {
 }
 
 reset() {
-    echo "CoreCell reset through GPIO$SX1302_RESET_PIN..."
+    echo "PineDIO reset through GPIO$SX1302_RESET_PIN..."
     # echo "SX1261 reset through GPIO$SX1302_RESET_PIN..."
-    # echo "CoreCell power enable through GPIO$SX1302_POWER_EN_PIN..."
-    #echo "CoreCell ADC reset through GPIO$AD5338R_RESET_PIN..."
+    # echo "PineDIO power enable through GPIO$SX1302_POWER_EN_PIN..."
+    #echo "PineDIO ADC reset through GPIO$AD5338R_RESET_PIN..."
 
-    # write output for SX1302 CoreCell power_enable and reset
+    # write output for SX1302 PineDIO power_enable and reset
     # echo "1" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value; WAIT_GPIO
 
     echo "1" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
